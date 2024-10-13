e67306abc85315d531eb4070b3c25aeb36a327c9
Wiritipon Thapkhan <81100736+zignalssss@users.noreply.github.com>
2024-10-13 23:55:08 +0700

    add turnleft and ultrasonic

--- src/robot1000s.ino
+++ src/robot1000s.ino
@@ -18,10 +18,9 @@
 #define MQTT_NAME "ESP32_1"
 #define MCP23017 0x20
 
-#define IN1 
-#define IN2 
-#define IN3 
-#define IN4 
+#define trigPin 13
+#define echoPin 27
+#define SOUND_SPEED 0.034
 
 //End Define Vaiable SECTION
 
@@ -74,7 +73,6 @@ void setDirectionOutputPortB() {
   Wire.endTransmission(); 
 }
 
-/*
 void front_motorStop() {
   Wire.beginTransmission(MCP23017);  
   Wire.write(0x12);              
@@ -89,32 +87,75 @@ void back_motorStop() {
 }
 
 void front_motorForward() {
+  Serial.println("TOGGG");
   Wire.beginTransmission(MCP23017);  
   Wire.write(0x12);              
-  Wire.write(0xA0); 
+  Wire.write(0x05); //reverse bit
   Wire.endTransmission();
 }
 void back_motorForward() {
   Wire.beginTransmission(MCP23017);  
   Wire.write(0x13);              
-  Wire.write(0xA0); 
+  Wire.write(0x05); 
+  Wire.endTransmission();
+}
+
+void front_motorbackForward() {
+  Serial.println("TOGGG");
+  Wire.beginTransmission(MCP23017);  
+  Wire.write(0x12);              
+  Wire.write(0x0A); //reverse bit
+  Wire.endTransmission();
+}
+void back_motorbackForward() {
+  Wire.beginTransmission(MCP23017);  
+  Wire.write(0x13);              
+  Wire.write(0x0A); 
   Wire.endTransmission();
 }
 
 void front_motorTurnleft() {
   Wire.beginTransmission(MCP23017);  
   Wire.write(0x12);              
-  Wire.write(0x10); 
+  Wire.write(0x04); 
   Wire.endTransmission();
 }
 
 void back_motorTurnleft() {
   Wire.beginTransmission(MCP23017);  
-  Wire.write(0x12);              
-  Wire.write(0x10); 
+  Wire.write(0x13);              
+  Wire.write(0x04); 
   Wire.endTransmission();
 }
-*/
+
+float distanceCM_cal(long duration){
+  return duration * SOUND_SPEED/2;
+}
+void ultraRead_Front(){
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  long duration = pulseIn(echoPin, HIGH);
+  
+  // Calculate the distance
+  float distanceCm = distanceCM_cal(duration);
+
+  
+  // Prints the distance in the Serial Monitor
+  Serial.print("Distance (cm): ");
+  Serial.println(distanceCm);
+  delay(1000);
+}
+
+void ultraRead_Right(){
+
+}
 
 void setup() {
   Serial.begin(115200);
@@ -124,16 +165,9 @@ void setup() {
   //I2C SETUP
   setDirectionOutputPortA();
   setDirectionOutputPortB();
-  //SETUP OUTPUT
 
-
-
-  //End SETUP OUTPUT
-
-  //SETUP INPUT
-
-
-  //End SETUP INPUT
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
 
   //MQTT Setup Section
   Serial.print("Connecting to ");
@@ -166,15 +200,14 @@ void loop() {
     mqtt.loop();
   }
   if(isToggle){
-    Serial.println("TOGGG");
-    Wire.beginTransmission(MCP23017);  
-    Wire.write(0x12);              
-    Wire.write(0x05); //reverse bit
-    Wire.endTransmission();
+    ultraRead_Front();
+    //front_motorForward();
+    //back_motorForward();
+    //front_motorTurnleft();
+    //back_motorTurnleft();
   }else{
-     Wire.beginTransmission(MCP23017);  
-    Wire.write(0x12);              
-    Wire.write(0x00); 
-    Wire.endTransmission();
+    front_motorStop();
+    back_motorStop();
+
   }
 }
